b9e1a5241704d086c5b1622e7af1b6b15e8f670c
Pyotr Pichugin <p.pichugin@andersenlab.com>
2023-08-19 18:25:31 +0500

    HW07: 2nd variant

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,59 @@
 mod smart_socket;
+mod tcp_server;
 
-use std::io::{Write};
-use std::net::{TcpListener, TcpStream};
+use rand::Rng;
+use std::io::Write;
+use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
 
-const ADDR: &str = "192.168.0.103:8095";
+const ADDR: &str = "127.0.0.1:8095";
 
 fn main() {
     let server_thread = thread::spawn(|| {
-        start_tcp_server();
+        tcp_server::start_tcp_server();
     });
 
-    // Create a SmartSocketClient instance
-    let mut socket_client = smart_socket::SmartSocketClient::new(ADDR);
-
-    let simulation_thread = thread::spawn(move || {
-        simulate_smart_socket_behavior(&mut socket_client);
+    let client_thread = thread::spawn(|| {
+        simulate_smart_socket_behavior();
     });
 
     server_thread.join().unwrap();
-    simulation_thread.join().unwrap();
+    client_thread.join().unwrap();
 }
 
-fn start_tcp_server() {
-    let listener = TcpListener::bind(ADDR).expect("Failed to bind");
-    println!("TCP server started");
+fn simulate_smart_socket_behavior() {
+    let mut socket_client = TcpStream::connect(ADDR).expect("Failed to connect to server");
+    println!(
+        "Socket client started: {}",
+        socket_client.peer_addr().unwrap()
+    );
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                thread::spawn(move || {
-                    handle_client(stream);
-                });
-            }
-            Err(e) => {
-                eprintln!("Error: {}", e);
-            }
-        }
-    }
-}
-
-fn handle_client(mut stream: TcpStream) {
-    let imitation_response = "Power: On\nConsumption: 100W";
-    let imitation_socket = smart_socket::ImitationSmartSocket::new(imitation_response);
-
-    for _ in 0..5 {
-        let response = imitation_socket.get_info();
-        stream.write_all(response.as_bytes()).expect("Failed to send response");
+    for ind in 0..5 {
+        let command = if rand::thread_rng().gen_bool(0.5) {
+            "turn_on"
+        } else {
+            "turn_off"
+        };
+        println!("{}: Sending command '{}'... ", ind, command);
+        socket_client
+            .write_all(command.as_bytes())
+            .expect("Failed to send command");
+        socket_client.flush().expect("Failed to flush");
         thread::sleep(Duration::from_secs(2));
     }
-}
 
-fn simulate_smart_socket_behavior(socket_client: &mut smart_socket::SmartSocketClient) {
-    if let Err(err) = socket_client.connect() {
-        eprintln!("Error connecting to the smart socket: {}", err);
-        return;
-    }
+    // test unknown command
+    socket_client
+        .write_all("ABCD".as_bytes())
+        .expect("Failed to send command");
+    socket_client.flush().expect("Failed to flush");
+    thread::sleep(Duration::from_secs(2));
 
-    for _ in 0..5 {
-        if let Err(err) = socket_client.send_command("turn_on") {
-            eprintln!("Error sending command: {}", err);
-            return;
-        }
-
-        println!("Smart socket is on");
-
-        thread::sleep(Duration::from_secs(2));
-
-        if let Err(err) = socket_client.send_command("turn_off") {
-            eprintln!("Error sending command: {}", err);
-            return;
-        }
-
-        println!("Smart socket is off");
-
-        thread::sleep(Duration::from_secs(2));
-    }
+    // close the connection
+    println!(
+        "Socket client {} finished",
+        socket_client.peer_addr().unwrap()
+    );
+    drop(socket_client);
 }
--- src/smart_socket.rs
+++ src/smart_socket.rs
@@ -1,55 +1,31 @@
-use std::io::{Read, Write};
-use std::net::TcpStream;
-
-pub struct SmartSocketClient {
-    address: String,
-    stream: Option<TcpStream>,
-}
-
-impl SmartSocketClient {
-    pub fn new(address: &str) -> Self {
-        SmartSocketClient {
-            address: address.to_string(),
-            stream: None,
-        }
-    }
-
-    pub fn connect(&mut self) -> Result<(), std::io::Error> {
-        let stream = TcpStream::connect(&self.address)?;
-        self.stream = Some(stream);
-        Ok(())
-    }
-
-    pub fn send_command(&mut self, command: &str) -> Result<String, std::io::Error> {
-        if let Some(stream) = &mut self.stream {
-            stream.write_all(command.as_bytes())?;
-            stream.flush()?; // Flush the stream to ensure data is sent
-
-            let mut response = String::new();
-            stream.read_to_string(&mut response)?;
-
-            Ok(response)
-        } else {
-            Err(std::io::Error::new(
-                std::io::ErrorKind::NotConnected,
-                "Socket not connected",
-            ))
-        }
-    }
-}
-
 pub struct ImitationSmartSocket {
-    response: String,
+    pub(crate) response: String,
+    is_on: bool,
 }
 
 impl ImitationSmartSocket {
     pub fn new(response: &str) -> Self {
         ImitationSmartSocket {
             response: response.to_string(),
+            is_on: false, // Изначально умная розетка выключена
         }
     }
 
-    pub fn get_info(&self) -> &str {
-        &self.response
+    pub fn turn_on(&mut self) {
+        self.is_on = true;
+        self.response = self.get_info();
+    }
+
+    pub fn turn_off(&mut self) {
+        self.is_on = false;
+        self.response = self.get_info();
+    }
+
+    pub fn get_info(&self) -> String {
+        format!(
+            "Power: {}. Consumption: {}W.\n",
+            if self.is_on { "On" } else { "Off" },
+            if self.is_on { 100 } else { 0 } // Имитируем потребление мощности
+        )
     }
 }
