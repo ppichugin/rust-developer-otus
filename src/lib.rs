ef33f781c8a730cad067da6729952d6be89092e1
Pyotr Pichugin <p.pichugin@andersenlab.com>
2023-09-13 22:44:16 +0500

    Fix: cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 pub mod dynamicv;
 pub mod staticv;
 
-const JUNIOR_CLASS: & str = "• Writing some trait...";
-const JUNIOR_DB: & str = "• Trying to query DB...";
-const JUNIOR_TEST: & str = "• Creating simple test...";
+const JUNIOR_CLASS: &str = "• Writing some trait...";
+const JUNIOR_DB: &str = "• Trying to query DB...";
+const JUNIOR_TEST: &str = "• Creating simple test...";
 
-const SENIOR_CLASS: & str = "• Improving traits...";
-const SENIOR_DB: & str = "• Fixing query to DB...";
-const SENIOR_TEST: & str = "• Optimizing test...";
+const SENIOR_CLASS: &str = "• Improving traits...";
+const SENIOR_DB: &str = "• Fixing query to DB...";
+const SENIOR_TEST: &str = "• Optimizing test...";
 
 pub struct Junior;
 pub struct Senior;
